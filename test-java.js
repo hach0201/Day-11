2325073da0eda2bafe6d473b84222de7ad2bc4be
hach0201 <102880420+hach0201@users.noreply.github.com>
2023-08-23 19:36:39 +0100

    Add files via upload

---
+++ test-java.js
@@ -0,0 +1,48 @@
+console.log("hello world");
+
+var age = 23;
+
+var isStudent = true;
+
+var favoriteColors = ["yellow", "purple", "black"];
+
+var testString = 'This is a string with "single" and \'double\' quotes.';
+
+console.log(testString);
+
+var name = "charrah hajar"; 
+var studentStatus = isStudent ? "a student" : "not a student";
+var combine = "My name is " + name + ". I am " + age + " years old and I am " + studentStatus + ".";
+
+console.log(combine);
+
+var myfavAnimal="wolf";
+var myfavColor="yellow";
+
+var yourFavAnimal= 'monkey';
+var yourfavColor= 'black';
+
+if (myfavAnimal===yourFavAnimal){
+console.log("the same choice.");
+}
+else{ console.log("not the same choice.");
+}
+
+var mynumber=12;
+
+if (mynumber>0) {
+    console.log("positive number.");
+} else if (mynumber<0) {
+    console.log("negative number.");
+} else {
+    console.log("number is zero");
+}
+
+var valeur = "hajar";
+
+if (valeur) {
+  console.log("la valeur is truthy.");
+} else {
+  console.log("la valeur is falsy.");
+}
+
